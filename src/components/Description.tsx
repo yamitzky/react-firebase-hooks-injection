97d6775782c4d6da845c9d56a645160065c859f0
Mitsuki Ogasahara <negiga@gmail.com>
2020-08-07 05:25:14 +0900

    説明書きを追加

---
+++ src/components/Description.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Description: React.FC = () => {
+  return (
+    <p>
+      ソースコード:
+      <a href="https://github.com/yamitzky/react-firebase-hooks-injection">
+        GitHub
+      </a>
+    </p>
+  )
+}
--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -3,13 +3,16 @@ import { RouteComponentProps } from '@reach/router'
 import { TodoList } from '~/src/components/TodoList'
 import { UserHeader } from '~/src/components/UserHeader'
 import { AddTodo } from '~/src/components/AddTodo'
+import { Description } from '~/src/components/Description'
 
 export const TodosPage: React.FC<RouteComponentProps> = () => {
   return (
     <div>
       <UserHeader />
+      <h1>TODOリスト</h1>
       <TodoList />
       <AddTodo />
+      <Description />
     </div>
   )
 }
